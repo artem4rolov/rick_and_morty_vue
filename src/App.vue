b03d5b432bc578e9c7b285708e98649124c7bfdf
Artem <artem4rolov@gmail.com>
2023-03-12 15:35:59 +0300

    done

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,75 @@
 <template>
   <div id="app">
     <div v-for="char of characters" :key="char.id">
-      <CharCard :char="char" />
+      <CharCard v-if="characters" :char="char" />
     </div>
-    <PaginationMenu :pagination="pagination" />
+    <div v-if="pagination !== null" class="pagination__wrapper">
+      <button @click="getCharacters(pagination.startUrl)" class="start">
+        1
+      </button>
+      <button>...</button>
+      <button @click="getCharacters(pagination.prevPageUrl)" class="page">
+        {{ pagination.prevPageNumber }}
+      </button>
+      <button class="currentPage">
+        {{
+          pagination.nexPageNumber - 1 === -1
+            ? 42
+            : pagination.nexPageNumber - 1
+        }}
+      </button>
+      <button @click="getCharacters(pagination.nexPageUrl)" class="page">
+        {{ pagination.nexPageNumber }}
+      </button>
+      <button>...</button>
+      <button @click="getCharacters(pagination.endUrl)" class="end">42</button>
+    </div>
   </div>
 </template>
 
 <script>
 import CharCard from "./components/CharCard.vue";
-import PaginationMenu from "./components/PaginationMenu.vue";
 
 export default {
   name: "App",
   data() {
     return {
-      characters: [],
+      characters: null,
       pagination: null,
     };
   },
   components: {
     CharCard,
-    PaginationMenu,
   },
   mounted() {
     this.getCharacters();
   },
   methods: {
-    async getCharacters() {
-      await fetch("https://rickandmortyapi.com/api/character")
+    async getCharacters(page_url) {
+      page_url = page_url || "https://rickandmortyapi.com/api/character";
+
+      await fetch(page_url)
         .then((res) => res.json())
         .then((data) => {
           this.characters = data.results;
-          this.pagination = data;
+          this.makePagination(data.info);
         })
         .catch((err) => console.log(err));
+    },
+    makePagination(data) {
+      // объект пагинации со страницами
+      let pagination = {
+        startUrl: "https://rickandmortyapi.com/api/character",
+        endUrl: "https://rickandmortyapi.com/api/character?page=42",
+        currentPageUrl: data,
+        currentPageNumber: data,
+        nexPageUrl: data.next ? data.next : null,
+        nexPageNumber: data.next ? data.next.split("=")[1] : null,
+        prevPageUrl: data.prev ? data.prev : null,
+        prevPageNumber: data.prev ? data.prev.split("=")[1] : null,
+      };
+
+      this.pagination = pagination;
     },
   },
 };
@@ -50,6 +85,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
   font-family: "Roboto Condensed", sans-serif;
+  margin-bottom: 100px;
 
   display: grid;
   grid-template-columns: repeat(2, 300px);
@@ -58,10 +94,58 @@ export default {
   gap: 30px;
 }
 
-@media (max-width: 768px) {
-  #app {
-    grid-template-columns: repeat(2, 300px);
-  }
+.pagination__wrapper {
+  position: fixed;
+  width: 100vw;
+  height: 60px;
+  bottom: -1px;
+  left: 0;
+  background: #000000;
+  opacity: 0.9;
+
+  display: flex;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  gap: 15px;
+}
+
+button {
+  border: none;
+  color: #fff;
+  background: transparent;
+}
+
+.start {
+  cursor: pointer;
+}
+
+.start:hover {
+  opacity: 0.6;
+}
+
+.page {
+  font-size: 12px;
+  cursor: pointer;
+}
+
+.page:hover {
+  opacity: 0.6;
+}
+
+.currentPage {
+  font-size: 18px;
+  font-weight: 700;
+  cursor: pointer;
+  margin-bottom: 2px;
+}
+
+.end {
+  cursor: pointer;
+}
+
+.end:hover {
+  opacity: 0.6;
 }
 
 @media (max-width: 768px) {
--- src/components/PaginationMenu.vue
+++ src/components/PaginationMenu.vue
@@ -1,22 +1,98 @@
 <template>
-  <div class="pagination__wrapper"></div>
+  <div class="pagination__wrapper">
+    <button class="start">1</button>
+    <button>...</button>
+    <button class="page">Предыдущая</button>
+    <button class="currentPage">Текущая</button>
+    <button class="page">Следующая</button>
+    <button>...</button>
+    <button class="end">42</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "PaginationMenu",
-  props: ["pagination"],
-  mounted() {
-    this.getPaginationData();
+  props: {
+    pagination: Object,
   },
-  methods: {
-    getPaginationData() {
-      if (this.pagination) {
-        console.log(this.pagination);
-      }
-    },
+  data() {
+    return {
+      paginationData: null,
+    };
+  },
+  async mounted() {
+    this.paginationData = await this.pagination;
+    console.log(this.paginationData.nexPage.split("=")[1]);
+    console.log(this.paginationData.prevPage.split("=")[1]);
   },
 };
 </script>
 
-<style></style>
+<style>
+.pagination__wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 15px;
+}
+
+button {
+  border: none;
+  color: #fff;
+  background: transparent;
+}
+
+.start {
+  cursor: pointer;
+}
+
+.start:hover {
+  opacity: 0.6;
+}
+
+.page {
+  font-size: 14px;
+  cursor: pointer;
+}
+
+.page:hover {
+  opacity: 0.6;
+}
+
+.currentPage {
+  font-size: 18px;
+  font-weight: 700;
+  cursor: pointer;
+}
+
+.currentPage:hover {
+  opacity: 0.6;
+}
+
+.end {
+  cursor: pointer;
+}
+
+.end:hover {
+  opacity: 0.6;
+}
+
+@media (max-width: 768px) {
+  .pagination__wrapper {
+    gap: 10px;
+  }
+}
+
+@media (max-width: 480px) {
+  .pagination__wrapper {
+    gap: 10px;
+  }
+}
+
+@media (max-width: 320px) {
+  .pagination__wrapper {
+    gap: 10px;
+  }
+}
+</style>
